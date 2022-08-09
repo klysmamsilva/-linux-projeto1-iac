ef90866fd989d3b53b09ebb19009d5cf45b5efec
klysmamsilva <87070813+klysmamsilva@users.noreply.github.com>
2022-08-09 17:27:51 -0300

    Update iac1.sh

--- iac1.sh
+++ iac1.sh
@@ -5,6 +5,7 @@ echo "Criando diretórios..."
 mkdir /publico
 mkdir /adm
 mkdir /ven
+mkdir /sec
 
 echo "Criando grupos de usuários..."
 
@@ -14,17 +15,17 @@ groupadd GRP_SEC
 
 echo "Criando usuários..."
 
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd luke -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd kylo -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd rey -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 
-useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd leia -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd finn -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd rose -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
 
-useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd snoke -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd hux -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd phasma -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 
 echo "Especificando permissões dos diretórios...."
 
@@ -32,9 +33,9 @@ chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
 chown root:GRP_SEC /sec
 
-chmod 770 /adm
-chmod 770 /ven
-chmod 770 /sec
-chmod 777 /publico
+chmod 880 /adm
+chmod 880 /ven
+chmod 880 /sec
+chmod 888 /publico
 
 echo "Fim....."
